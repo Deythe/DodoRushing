24e962ec69e5244f19636703b8e02f756b946e02
Luac <lo.accard@gmail.com>
2023-01-28 19:32:31 +0100

    Player death

--- DodoRushing/Assets/Scripts/PlayerController.cs
+++ DodoRushing/Assets/Scripts/PlayerController.cs
@@ -133,6 +133,13 @@ public class PlayerController : MonoBehaviour
         
         dashInCooldown = false;
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 7)
+        {
+            GameManager.instance.RestartGame();
+        }
+    }
 
 
     public void Reset()
